1f7d3415fdaefea70c795c41f5c7e6bdba7189dc
seetharamakash2026 <seetharamakash2026@serrahs.com>
2025-09-14 17:48:49 -0700

    delete unnecessary

--- .automation/README.md
+++
@@ -1,3 +0,0 @@
-# Internal Use
-
-Users should not utilize the files located in this directory, as they are intended to automate updating of the example projects.
--- java/PhoenixSysId/src/main/java/frc/robot/Constants.java
+++
@@ -1,6 +0,0 @@
-package frc.robot;
-
-public class Constants {
-    public final static String kCANbus = "canivore";
-    public final static int kTalonFxId = 18;
-}
